f7280cd16dad791fa5ef17a8593fd1d60fe01710
trungnguyenjnp-debug <trungnguyen.jnp@gmail.com>
2026-06-18 22:01:39 -0400

    Update letter content in main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Thay đổi nội dung búc thư ở đây
-var letterContent =" Happy Monthsary My Love! Vậy là đôi mình đã đi với nhau được 10 tháng rùi nàii. Tuy là 6 tháng qua mình đã phải ở xa nhau nhưng tình cảm tụi mình vẫn cứ thế mà lớn hơn từng ngày! Cảm ơn em đã ở bên cạnh anh trong suốt thời gian qua, cùng anh trải qua những chuyện vui buồn những vẫn ngồi lại cùng anh sau tất cả. Anh luôn hi vọng đôi mình sẽ vẫn cùng nhau đón hết những trạm kỉ niệm ở phía trước mãi em nhé! Anh Yêu Em <3"
+var letterContent =" Happy Monthsary My Love! Vậy là đôi mình đã đi với nhau được 10 tháng rùi nàii. Tuy là 6 tháng qua mình đã phải ở xa nhau nhưng tình cảm tụi mình vẫn cứ thế mà lớn hơn từng ngày! Cảm ơn em đã ở bên cạnh anh trong suốt thời gian qua, cùng anh trải qua những chuyện vui buồn nhưng vẫn ngồi lại cùng anh sau tất cả. Anh luôn hi vọng đôi mình sẽ vẫn cùng nhau đón hết những trạm kỉ niệm ở phía trước mãi em nhé! Anh Yêu Em <3"
 
 // Tốc độ viết chữ. Số càng nhỏ tốc độ càng nhanh. 50 là tốc độ khá phù hợp
 durationWrite = 50 
